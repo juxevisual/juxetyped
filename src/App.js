fca359c86d65e33387e4d5a82b159ddb9fe7aeeb
juxevisual <juxevisual@gmail.com>
2025-02-14 15:05:23 +0700

    change text

--- src/App.js
+++ src/App.js
@@ -8,31 +8,35 @@ function App() {
         <div className="flex flex-col space-y-8 items-start justify-start">
           <HandRaisedIcon className="w-20 text-juxe animate-[wiggle_1s_ease-in-out_infinite]" />
           <h1 className="font-silkscreen text-7xl font-bold text-juxe tracking-wide">
-            Panjul!
+            JUXE VISUAL
           </h1>
           <h3 className="font-silkscreen text-back text-5xl">
-            Namaku adalah{" "}
-            <strong className="bg-red-400 text-white">panjul</strong> sekian dan{" "}
-            <strong className="bg-green-400">terima</strong> kasih
+            We love{" "}
+            <strong className="bg-red-400 text-white">Graphic Design</strong>{" "}
+            and Eveything <strong className="bg-green-400">Visual</strong> ,
+            probably
           </h3>
           <div className="font-silkscreen text-5xl flex space-x-3 ">
-            <h4> Panggil aku </h4>
+            <h4> We do </h4>
             <strong className="bg-juxe px-1">
               <Typical
                 steps={[
-                  "Panjul",
+                  "Graphic",
                   2000,
-                  "Manujul",
+                  "Poster",
                   2000,
-                  "Sikujul",
+                  "Album",
                   2000,
-                  "Marijul",
+                  "Web",
+                  2000,
+                  "Everything",
                   2000,
                 ]}
                 wrapper="p"
                 loop={Infinity}
               />
             </strong>
+            Design
           </div>
         </div>
       </main>
